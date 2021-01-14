1251792726ee48212e1724bd5be6bc4955abad8b
theXarxes <mccube00@gmail.com>
2021-01-14 15:12:07 +0100

    poprawiony move

--- Model/src/main/java/model/Pawn.java
+++ Model/src/main/java/model/Pawn.java
@@ -2,7 +2,7 @@ package model;
 
 public class Pawn {
     private final int color;
-    private int traveled;
+    private int traveled = 0;
 
     public Pawn(int color) {
         this.color = color;
@@ -17,7 +17,7 @@ public class Pawn {
     }
 
     public void setTraveled(int traveled) {
-        this.traveled = traveled;
+        this.traveled += traveled;
     }
 
 }
--- View/src/main/java/Ludo/App.java
+++ View/src/main/java/Ludo/App.java
@@ -1,11 +1,12 @@
 package Ludo;
 
-import model.Board;
+import model.*;
 
 public class App {
 
     public static void main(String[] args){
-    Board board = new Board(4);
+        Board board = new Board(4);
+        Move move = new Move(board);
     }
 
 }
